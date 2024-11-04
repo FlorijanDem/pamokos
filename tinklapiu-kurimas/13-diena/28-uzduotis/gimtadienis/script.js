c0e257fc5b213c748e9be2e38645da0196faf0f4
FlorijanDem <florijan.dem@gmail.com>
2024-11-04 14:43:24 +0400

    Add gimtadienis

---
+++ tinklapiu-kurimas/13-diena/28-uzduotis/gimtadienis/script.js
@@ -0,0 +1,9 @@
+const cookiesFromTautvydas = parseInt(
+  prompt("Kiek sausainių iškepė Tautvydas?")
+);
+const cookiesFromFrends = parseInt(
+  prompt("Keli draugai dar atsinešė po tiek pat sausainių?")
+);
+const peopleInParty = parseInt(
+  prompt("Kiek žmonių iš viso dalyvavo šventėje?")
+);
