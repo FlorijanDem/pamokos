233e930ab454e1a9b68a1be8d8b1a08d96c0bb14
FlorijanDem <florijan.dem@gmail.com>
2024-11-04 12:45:18 +0400

    Change let to const

--- tinklapiu-kurimas/13-diena/28-uzduotis/pamoka/script.js
+++ tinklapiu-kurimas/13-diena/28-uzduotis/pamoka/script.js
@@ -1,8 +1,8 @@
-let day1 = parseInt(prompt("Kiek pamokų yra pirmadienį?", ""));
-let day2 = parseInt(prompt("Kiek pamokų yra antradienį?", ""));
-let day3 = parseInt(prompt("Kiek pamokų yra trečiadienį?", ""));
-let day4 = parseInt(prompt("Kiek pamokų yra ketvirtadienį?", ""));
-let day5 = parseInt(prompt("Kiek pamokų yra penktadienį?", ""));
+const day1 = parseInt(prompt("Kiek pamokų yra pirmadienį?", ""));
+const day2 = parseInt(prompt("Kiek pamokų yra antradienį?", ""));
+const day3 = parseInt(prompt("Kiek pamokų yra trečiadienį?", ""));
+const day4 = parseInt(prompt("Kiek pamokų yra ketvirtadienį?", ""));
+const day5 = parseInt(prompt("Kiek pamokų yra penktadienį?", ""));
 
 const week = day1 + day2 + day3 + day4 + day5;
 const weekInMinute = week * 45;
