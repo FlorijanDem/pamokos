c1a70c5d99161f1101c830e264d9d2308dcdb5ac
FlorijanDem <florijan.dem@gmail.com>
2024-11-06 14:39:21 +0400

    Add 4 array

---
+++ tinklapiu-kurimas/15-diena/arrays/4/script.js
@@ -0,0 +1,7 @@
+function sumNumbers(...(x, y , z)) {
+    sumNumbers([1, 4, 8])
+}
+
+
+console.log(sumNumbers([1, 4, 8]));
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax#A_better_apply
