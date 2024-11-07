376497eff597434760b5e1630b7aa19814155011
FlorijanDem <florijan.dem@gmail.com>
2024-11-07 18:07:31 +0200

    Done exercise

---
+++ tinklapiu-kurimas/14-diena/snaigesUzLango/script.js
@@ -0,0 +1,6 @@
+const inSecondsFall = 5;
+const timeOfFall = 3;
+let rezult = 0;
+for (let i = 1; i++) {
+    console.log(i)
+}
--- tinklapiu-kurimas/16-diena/33-uzduotis/5/script.js
+++ tinklapiu-kurimas/16-diena/33-uzduotis/5/script.js
@@ -1,11 +1,11 @@
 const signupForm = document.querySelector("#signup-form");
 const list = [];
+let i = 0;
 signupForm.addEventListener("submit", (event) => {
-    const task = document.querySelector("#task");
-    const valueOfData = task.value;
-    console.log(task.value)
-    event.preventDefault();
-    list.push(valueOfData);
-    document.getElementById("list").innerHTML = list;
-    console.log(list)
-})
+  event.preventDefault();
+  const task = document.querySelector("#task");
+  const valueOfData = task.value;
+  list.push(valueOfData);
+  document.getElementById(`list${i}`).innerHTML = list[i];
+  i++;
+});
