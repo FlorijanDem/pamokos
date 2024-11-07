5e6c5f28fe1bf5064096a26199f26f9b2a0078e1
FlorijanDem <florijan.dem@gmail.com>
2024-11-07 12:35:56 +0400

    Add a onclick color change exercise 1

---
+++ tinklapiu-kurimas/16-diena/33-uzduotis/1/script.js
@@ -0,0 +1,12 @@
+const paragraph = document.getElementById("paragraph");
+const yellowClick = () => {
+  document.getElementById("paragraph").style.color = "yellow";
+};
+
+const greenClick = () => {
+  document.getElementById("paragraph").style.color = "green";
+};
+
+const redClick = () => {
+  document.getElementById("paragraph").style.color = "red";
+};
