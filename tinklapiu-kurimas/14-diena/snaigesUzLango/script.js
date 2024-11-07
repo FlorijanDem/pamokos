d9a1d244d36fc0d573405bf3f683fe18700764f5
FlorijanDem <florijan.dem@gmail.com>
2024-11-07 18:41:34 +0200

    Done snaigesUzLango exercise

--- tinklapiu-kurimas/14-diena/snaigesUzLango/script.js
+++ tinklapiu-kurimas/14-diena/snaigesUzLango/script.js
@@ -1,6 +1,28 @@
-const inSecondsFall = 5;
-const timeOfFall = 3;
-let rezult = 0;
-for (let i = 1; i++) {
-    console.log(i)
+const InSecond = parseInt(
+  prompt("Įveskite, kiek snaigių nukrito per pirmąją sekundę")
+);
+const snowTime = parseInt(prompt("Įveskite, kiek sekundžių snigo"));
+
+x = InSecond;
+let Snowflakes = 0;
+
+for (let i = 0; i < snowTime; i++) {
+  if (i > 0) {
+    x = x * 2;
+  }
+  Snowflakes = Snowflakes + x;
 }
+
+console.log(Snowflakes);
+
+// const inSecondFall = 5;
+// const timeOfFall = 3;
+// let rezult = 0;
+// let x = inSecondFall;
+// let z = inSecondFall;
+// for (let i = 0; i <= timeOfFall; i++) {
+//     console.log(z)
+//     z = inSecondFall + x;
+//     x = x * 2;
+//     // console.log(x)
+// }
