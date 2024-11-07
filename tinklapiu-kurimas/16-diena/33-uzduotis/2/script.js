be50970f0154124319df99de6ca7686ee9d45ffd
FlorijanDem <florijan.dem@gmail.com>
2024-11-07 12:57:12 +0400

    Add (2) resize exercise

---
+++ tinklapiu-kurimas/16-diena/33-uzduotis/2/script.js
@@ -0,0 +1,6 @@
+const resizeToBig = () => {
+  document.getElementById("htmlFiveLogo").style.width = "300px";
+};
+const resizeToSmall = () => {
+  document.getElementById("htmlFiveLogo").style.width = "100px";
+};
