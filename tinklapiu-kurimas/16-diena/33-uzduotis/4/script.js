0247602313ac846dde034e06fc79fe8ed18cd883
FlorijanDem <florijan.dem@gmail.com>
2024-11-07 14:30:21 +0400

    Add guesse

--- tinklapiu-kurimas/16-diena/33-uzduotis/4/script.js
+++ tinklapiu-kurimas/16-diena/33-uzduotis/4/script.js
@@ -4,19 +4,22 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
+let guesses = 0;
+
 signupForm.addEventListener("submit", (event) => {
   event.preventDefault();
   x = parseInt(signupForm.elements["number"].value);
   y = getRandomInt(100);
+  guesses++;
   if (x === y) {
     document.getElementById("resultText").innerHTML = "You are won!";
   } else if (x < y) {
     document.getElementById("resultText").innerHTML =
       "The guessed number is bigger!";
-    document.getElementById("resultCount").innerHTML = y + " guesses was done";
+    document.getElementById("resultCount").innerHTML = guesses + " guesses was done";
   } else if (x > y) {
     document.getElementById("resultText").innerHTML =
       "The guessed number is smaller!";
-    document.getElementById("resultCount").innerHTML = y + " guesses was done";
+    document.getElementById("resultCount").innerHTML = guesses + " guesses was done";
   }
 });
