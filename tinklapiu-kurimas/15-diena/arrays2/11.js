85cd74a845919ebbe4b4d39755fbca14f736ea32
FlorijanDem <florijan.dem@gmail.com>
2024-11-09 17:51:57 +0200

    Add 11 array2 exercise

---
+++ tinklapiu-kurimas/15-diena/arrays2/11.js
@@ -0,0 +1,11 @@
+function glueArrays(array1, array2) {
+    inOne = array1 + array2;
+    return inOne;
+}
+
+
+console.log(glueArrays([1, 2, 3], [4, 5, 6]))
+console.log(glueArrays([-10, undefined], [true, 'waffles']))
+console.log(glueArrays([], []))
+console.log(glueArrays([20, 104], []))
+console.log(glueArrays([], ['hello', 'world']))
