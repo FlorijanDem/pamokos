1b5288d628e54245b046ab55a581c8dfa5d29cd2
FlorijanDem <florijan.dem@gmail.com>
2024-11-13 18:52:30 +0200

    Done 3 exercise

--- react/3/script.js
+++ react/3/script.js
@@ -1,11 +1,14 @@
-async function fetchUserData (id){
-    const url = await fetch("https://jsonplaceholder.typicode.com/users/")
-    url.then(res=>
-        res.json()).then(id => {
-            console.log(id)
-        })
-
+async function fetchUserData(userId) {
+  try {
+    const url = await fetch(
+      `https://jsonplaceholder.typicode.com/users/${userId}`
+    );
+    const data = await url.json();
+    findedName = "User name:" + data.name;
+    return findedName;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-console.log(fetchUserData(1))
-
+console.log(fetchUserData(1));
