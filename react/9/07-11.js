67e659c4fddec561f943a60e2b8d515a79fa3a85
FlorijanDem <florijan.dem@gmail.com>
2024-11-15 16:26:21 +0200

    Done exercise 07-11

--- react/9/07-11.js
+++ react/9/07-11.js
@@ -9,4 +9,12 @@ console.log(camelize("JavaScriptExercises"));
 "JavaScriptExercises"
 */
 
+function camelize(text) {
+  return text.replace(/[^a-zA-Z0-9]+(.)/g, function (match, chr) {
+    return chr.toUpperCase();
+  });
+}
 
+console.log(camelize("JavaScript Exercises"));
+console.log(camelize("JavaScript exercises"));
+console.log(camelize("JavaScriptExercises"));
