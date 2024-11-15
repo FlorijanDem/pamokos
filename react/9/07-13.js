8a8ad33e5a7d6a4c9434d53c6d70c785eeb35f35
FlorijanDem <florijan.dem@gmail.com>
2024-11-15 16:39:50 +0200

    Done exercise 07-13

--- react/9/07-13.js
+++ react/9/07-13.js
@@ -8,5 +8,18 @@ console.log(repeat('Ha!',3));
 "Ha!Ha!"
 "Ha!Ha!Ha!"
 */
+function repeat(text, x) {
+  let result = [];
+  if (x > 0) {
+    for (let i = 0; i < x; i++) {
+      result.push(text);
+    }
+    return result.join("");
+  } else {
+    return text;
+  }
+}
 
-
+console.log(repeat("Ha!"));
+console.log(repeat("Ha!", 2));
+console.log(repeat("Ha!", 3));
