14640eae8a24220d2475ee7daf0eba7218524973
FlorijanDem <florijan.dem@gmail.com>
2024-11-17 16:38:13 +0200

    Done 07-25.js

--- react/9/07-25.js
+++ react/9/07-25.js
@@ -7,24 +7,9 @@ Output :
 "SUadeeinsttt"
 */
 
-// Undone
 function alphabetize_string(text) {
-//   const arr = [...text];
-//   const buffer = arr.sort((a, b) => a.localeCompare(b));
-//   const result = [];
-//   const upperCases = [];
-//   let capitalCases = 0;
-//   for (let i = 0; i < buffer.length; i++) {
-//     if (buffer[i] == buffer[i].toUpperCase) {
-//         upperCases.push(buffer[i]);
-//         capitalCases++;
-//     }
-//   }
-//   console.log(upperCases)
-//   console.log(capitalCases)
-
-//   return result;
-
+  const result = text.split("").sort().join("").trim();
+  return result;
 }
 
 console.log(alphabetize_string("United States"));
