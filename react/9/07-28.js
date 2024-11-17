0136edba0976b482b591e42d8c4e1f37b6f1d5f8
FlorijanDem <florijan.dem@gmail.com>
2024-11-17 15:49:25 +0200

    Done 07-28.js

--- react/9/07-28.js
+++ react/9/07-28.js
@@ -8,3 +8,14 @@ Output :
 "100"
 */
 
+function hex_to_ascii(value) {
+  const hex = value.toString();
+  let result = "";
+  for (let i = 0; i < hex.length; i += 2) {
+    result += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+  }
+  return result;
+}
+
+console.log(hex_to_ascii("3132"));
+console.log(hex_to_ascii("313030"));
