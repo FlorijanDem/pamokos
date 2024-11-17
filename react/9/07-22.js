495be711a24fc1233240c57e7b1dc86b5dcd3613
FlorijanDem <florijan.dem@gmail.com>
2024-11-17 16:46:41 +0200

    Done 07-22.js

--- react/9/07-22.js
+++ react/9/07-22.js
@@ -8,28 +8,25 @@ Output :
 "xercises"
 */
 
-//Undone
-function subStrAfterChars (text, charterOne, type) {
-    // Using spread operator you can easy append every charted of the string
-    // to separete array value
-    arr = [...text];
-    buffer = [];
-    if (type === "a") {
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i] == charterOne) {
-                return buffer.join("")
-            }
-            buffer.push(arr[i]);
-        }
-    } else if (type === "b") {
-        for (let i = arr.length; i > 0; i--) {
-            if (arr[i] == charterOne) {
-                return buffer.join("")
-            }
-            buffer.push(arr[i]);
-        }
+function subStrAfterChars(text, charterOne, type) {
+  arr = [...text];
+  buffer = [];
+  if (type === "a") {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] == charterOne) {
+        return buffer.join("");
+      }
+      buffer.push(arr[i]);
     }
+  } else if (type === "b") {
+    for (let i = arr.length; i > 0; i--) {
+      if (arr[i] == charterOne) {
+        return buffer.reverse().join("");
+      }
+      buffer.push(arr[i]);
+    }
+  }
 }
 
-console.log(subStrAfterChars('w3resource: JavaScript Exercises', ':','a'));
-console.log(subStrAfterChars('w3resource: JavaScript Exercises', 'E','b'));
+console.log(subStrAfterChars("w3resource: JavaScript Exercises", ":", "a"));
+console.log(subStrAfterChars("w3resource: JavaScript Exercises", "E", "b"));
