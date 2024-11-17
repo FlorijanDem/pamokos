c56f2c77a5a623def132fe2c96d7cc5f2318901d
FlorijanDem <florijan.dem@gmail.com>
2024-11-17 16:19:00 +0200

    Done 07-32.js

--- react/9/07-32.js
+++ react/9/07-32.js
@@ -4,3 +4,10 @@ Test Data :
 console.log(remove_non_ascii('äÄçÇéÉêPHP-MySQLöÖÐþúÚ'));
 "PHP-MySQL"
 */
+
+function remove_non_ascii(text) {
+  const result = text.replace(/[^\x20-\x7E]/g, "");
+  return result;
+}
+
+console.log(remove_non_ascii("äÄçÇéÉêPHP-MySQLöÖÐþúÚ"));
