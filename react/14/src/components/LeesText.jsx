baf523a6721885f47c198818ff73de3e219eea26
FlorijanDem <florijan.dem@gmail.com>
2024-11-20 11:02:46 +0400

    Some fixes

--- react/14/src/components/LeesText.jsx
+++ react/14/src/components/LeesText.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-export default function LessText (props) {
-    const [less, setLess] = useState(true);
-    console.log(props)
-    // return (
-        
-    //     <p>
-    //         {less ? props.text}
-    //     </p>
-        
-    // )
+export default function LessText(props) {
+  const [less, setLess] = useState(true);
+  return (
+    <p>
+      {less ? props.text.substring(0, props.defaultLength) : props.text}{" "}
+      <a
+        href="javascript:;"
+        onClick={() => {
+          setLess(!less);
+        }}
+      >
+        {less ? ":more text" : ":less text"}
+      </a>
+    </p>
+  );
 }
