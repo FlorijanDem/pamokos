800abd64c0fcfc6828cc6baf93c3b101478677b6
FlorijanDem <florijan.dem@gmail.com>
2024-11-20 19:14:15 +0200

    Complete exercise 14

--- react/14/src/App.jsx
+++ react/14/src/App.jsx
@@ -10,6 +10,10 @@ and just keep taking the next step
 towards completing it."
         defaultLength={45}
       />
+      <LessText
+        text="The etymology of the various words for tea reflects the history of transmission of tea drinking culture and trade from China to countries around the world."
+        defaultLength={43}
+      />
     </>
   );
 }
