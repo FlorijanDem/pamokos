7d883fc5766ff675ea29863553ef805274368492
FlorijanDem <florijan.dem@gmail.com>
2025-01-27 14:50:45 +0400

    Remowe comments

--- express/node-express-atsiskaitimas/controllers/bookController.js
+++ express/node-express-atsiskaitimas/controllers/bookController.js
@@ -1,22 +1,3 @@
-// const { getAll } = require("../models/Model");
-// const AppError = require("../utils/appError");
-
-// exports.getAll = async (req, res, next) => {
-//   try {
-//     const data = await getAll();
-
-//     if (!data || data.length === 0) throw new AppError("No data found", 404);
-
-//     res.status(200).json({
-//       status: "success",
-//       data,
-//     });
-//   } catch (err) {
-//     next(err);
-//   }
-// };
-
-// controllers/todoController.js
 const Book = require('../models/Model');
 
 exports.getAll = function(req, res) {
